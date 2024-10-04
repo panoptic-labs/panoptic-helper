681ff41644920d069cba647872084637f99f1df4
Guillaume Lambert <guil.lambert@protonmail.com>
2024-10-04 15:14:46 -0400

    fix: updated to latest panoptic-v1-core version

--- test/PanopticHelper.t.sol
+++ test/PanopticHelper.t.sol
@@ -1281,14 +1281,13 @@ contract PanopticHelperTest is PositionUtils {
             );
 
             (calculatedCollateralBalance, calculatedRequiredCollateral) = PanopticMath
-                .convertCollateralData(tokenData0, tokenData1, returnTokenType ? 1 : 0, atTick);
+                .getCrossBalances(tokenData0, tokenData1, Math.getSqrtRatioAtTick(atTick));
 
             // these are the balance/required cross, reusing variables to save stack space
             (collateralBalance, requiredCollateral) = ph.checkCollateral(
                 pp,
                 Alice,
                 atTick,
-                returnTokenType ? 1 : 0,
                 posIdList
             );
 
