b2190cec49481290ad3e4bc07fe52c496a3050a5
Suthan Somadeva <106453938+PeakyCryptos@users.noreply.github.com>
2024-11-11 21:42:38 -0500

    put: revert and correct

--- src/PanopticHelper.sol
+++ src/PanopticHelper.sol
@@ -551,8 +551,8 @@ contract PanopticHelper {
                     // resultant amounts returned in terms of token 0
                     uint256 amount0Delta = Math.mulDiv(
                         currentLiquidity << 96,
-                        lowPriceX96 - currentPriceX96,
-                        lowPriceX96
+                        currentPriceX96 - lowPriceX96,
+                        currentPriceX96
                     ) / lowPriceX96;
 
                     console2.log("amount 0 delta", amount0Delta);
